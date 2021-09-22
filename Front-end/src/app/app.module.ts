d4eccb23b05aa0950398c3b6385a8ff17188c039
klineconnor <klineconnor@yahoo.com>
2021-09-22 13:42:38 -0500

    added signup component and fixed readme

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ README.md
@@ -0,0 +1,5 @@
+#Library Management System
+Insert summary here
+
+#Front End
+Run using ng serve -o in command line
